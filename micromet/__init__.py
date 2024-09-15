b964b013a8b0375584ea6a0b008aff2f03debd73
Paul <paulinkenbrandt@utah.gov>
2024-09-14 21:30:26 -0600

    Add Flux variables CSV and enhance micromet functionality
    Added a comprehensive CSV file for Flux variables, enhancing data tracking. Updated multiple scripts in the micromet package to improve configuration handling, CSV path management, and file compilation. Moreover, introduced new dependencies in the environment file and pyproject.toml to support enhanced data visualization and statistical analysis features.

--- micromet/__init__.py
+++ micromet/__init__.py
@@ -1,7 +1,8 @@
 from .ec import CalcFlux
 
 # from .Larry_Flux_Calc import *
-
+from .cs_wrapper import *
+from .licor_wrapper import *
 from .converter import *
 from .tools import *
 __version__ = '0.2.0'
